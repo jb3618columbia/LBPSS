a20895ee5e8b17ec1a694c500ecfb4b988610cc6
ffagan <Francois@dyn-129-236-212-76.dyn.columbia.edu>
2016-09-12 21:23:07 -0400

    updated boltzmann

--- generalized_boltz_inference.m
+++ generalized_boltz_inference.m
@@ -1,25 +1,39 @@
 % Runs experiments for general Boltzman machine
+% Based on "Bayesian Learning in Undirected Graphical Models: Approximate
+% MCMC algorithms" (BLUG)
+
+% The goal is to sample W using Metropolis Hastings from its posterior.
+% Each iteration requires calculating a constant Z(W) which can be
+% approximated using MCMC methods if it cannot be calculated exactly by brute force.
 
 % Load dataset (available from http://artax.karlin.mff.cuni.cz/r-help/library/bnlearn/html/coronary.html)
 load('coronary.mat')
 
 % Parameters
-n_iterations = 100;
-n_samples_per_iteration = 10;
-d_s = 5;
-d_x = size(coronary,2);
-W_sigma_2 = 1000;
+n = size(coronary,1);       % number of training examples
+d = size(coronary,2);       % dimension of S
+K = 10;                     % number of MCMC samples per F calcuation
 
 %% Run samplers
 
 % Initialize
-s = zeros(1 , d_s);
-W = zeros(d_s + d_x , d_s + d_x);
+W = zeros(d,d);
 
 % Metropolis-Hastings sampler
+
 % 1. Propose new W' from a symmetric proposal distribution p(W'|W)
-% 2. Sample N new S' from p(S'|W') using MCMC methods
-% 3. Compute "joint" distribution: J' = \frac{1}{N} * sum_{i=1}^N p(S',W')
-% 4. Accept with probability p(W') J' / (p(W) + J). Here p(W) is the prior
-% on W. Note that J should not be recomputed each iteration, only J' should
-% be.
+
+% 2. Calculate F = Z(W)/Z(W') ~ \frac{1}{K} * \sum_{k=1}^K exp(s_k(W-W')*s_k) 
+%    where we have sampled K values of S from p(S|W') using an MCMC method.
+%    This is equation (8) of BLUG.
+%    Note for the exact case instead of sampling we can calculate
+%    F = Z(W)/Z(W') directly using brute force. 
+
+% 3. Accept W' with probability p(W') / p(W) * F^N * exp(\sum_{i=1}^n s_i(W-W')*s_i)
+%    where p(W) is the prior on W and s_i are the datapoints with i=1,...,n.
+%    This is equation (7) of BLUG.
+
+%% Plots
+
+% Plot empirical distributions of W for the exact sampler vs approximate
+% samplers.
