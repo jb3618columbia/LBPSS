8f962745e275e50fc541ca82787d701a4ecf192f
jalaj-columbia <Jalaj@dyn-160-39-210-128.dyn.columbia.edu>
2016-09-14 00:23:00 -0400

    Cleaner empirical counts.

--- empericalCounts.m
+++ empericalCounts.m
@@ -20,14 +20,20 @@ K = size(a,2)/2;
 count = zeros(K, 4);
 for k=1:K
    i = a(2*k -1); j=a(2*k);
-   m = samples(i, :) + samples(j,:);
-   count(k,1) = size(m(m==2),2);
-   count(k,4) = size(m(m==-2),2);
    
-   n = samples(i, :) - samples(j,:);
-   count(k,2) = size(n(n==-2),2);
-   count(k,3) = size(n(n==2),2);
+   count(k,:) = [ sum( (samples(i,:) > 0) .* (samples(j,:) > 0) ), ....
+                sum( (samples(i,:) < 0) .* (samples(j,:) > 0) ), ...
+                sum( (samples(i,:) > 0) .* (samples(j,:) < 0) ), ....
+                sum( (samples(i,:) < 0) .* (samples(j,:) < 0) ), ]/size(samples,2);
+   
+%    m = samples(i, :) + samples(j,:);
+%    count(k,1) = size(m(m==2),2);
+%    count(k,4) = size(m(m==-2),2);
+%    
+%    n = samples(i, :) - samples(j,:);
+%    count(k,2) = size(n(n==-2),2);
+%    count(k,3) = size(n(n==2),2);
    
 end
 
-count = count/size(samples,2);
+% count = count/size(samples,2);
